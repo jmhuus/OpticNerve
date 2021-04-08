fef9990701364f5a63178fcd711a0bc446cba2c3
Jordan Huus <jordanhuusi@gmail.com>
2021-04-07 22:40:16 -0700

    Fixed initial display value for f-stop.

--- src/app/capture/components/capture-control/capture-control.component.ts
+++ src/app/capture/components/capture-control/capture-control.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Device } from '../../device';
+import { interval } from 'rxjs';
 
 export interface Tile {
     color: string;
@@ -40,14 +41,32 @@ export class CaptureControlComponent implements OnInit {
         if (this.chosenDevice) {
             this.createFormControls();
             this.createForm();
+
+            // F-number takes a while to set, wait until ready
+            let subscription = interval(1000).subscribe(() => {
+                if (this.chosenDevice) {
+                    if (this.chosenDevice.fNumber) {
+                        this.fNumber = new FormControl(
+                            this.chosenDevice.fNumber, Validators.required);
+                        this.fNumber.valueChanges.subscribe(val => {
+                            this.chosenDevice.fNumber = val;
+                            this.chosenDevice.setFNumber();
+                        });
+                        this.form.addControl('fNumber', this.fNumber);
+                        subscription.unsubscribe();
+                    }
+                }
+            });
         }
     }
 
     createFormControls(): void {
-        this.exposure = new FormControl(this.chosenDevice.exposureTime, Validators.required);
-        this.iso = new FormControl(this.chosenDevice.iso, Validators.required);
-        this.fNumber = new FormControl(this.chosenDevice.fNumber, Validators.required);
-        this.captureCount = new FormControl(this.chosenDevice.captureCount, Validators.required);
+        this.exposure = new FormControl(
+            this.chosenDevice.exposureTime, Validators.required);
+        this.iso = new FormControl(
+            this.chosenDevice.iso, Validators.required);
+        this.captureCount = new FormControl(
+            this.chosenDevice.captureCount, Validators.required);
 
         this.exposure.valueChanges.subscribe(val => {
             this.chosenDevice.exposureTime = val;
@@ -60,18 +79,15 @@ export class CaptureControlComponent implements OnInit {
         this.captureCount.valueChanges.subscribe(val => {
             this.chosenDevice.captureCount = val;
         });
-        this.fNumber.valueChanges.subscribe(val => {
-            this.chosenDevice.fNumber = val;
-            this.chosenDevice.setFNumber();
-        });
+
     }
 
     createForm(): void {
         this.form = new FormGroup({
             captureCount: this.captureCount,
             exposure: this.exposure,
-            iso: this.iso,
-            fNumber: this.fNumber
+            iso: this.iso
+            // fNumber: this.fNumber
         });
     }
 
