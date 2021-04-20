a10dad4d00fb7a83b430b94fc1b015218afa1fba
Jordan Huus <jordanhuusi@gmail.com>
2021-04-19 21:08:16 -0700

    Added license, description, and icon to package.json. Added consistent formatting to main.js.

--- main.js
+++ main.js
@@ -22,13 +22,13 @@ function createWindow () {
 	    sandbox: false,
 	    nodeIntegration: true
 	}
-    })
+    });
 
     // and load the index.html of the app.
-    mainWindow.loadFile('./dist/OpticNerve/index.html')
+    mainWindow.loadFile(path.join(__dirname, '/dist/OpticNerve/index.html'));
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools();
 
     webContents = mainWindow.webContents;
 }
@@ -37,24 +37,23 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow()
-    
+    createWindow();
 
     app.on('activate', function () {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+    });
+});
 
 // Init Python Flask server to handle device nication
 function initPythonServer() {
-    child = spawn(path.join(__dirname, "./backend/dist/server/server"));
+    child = spawn(path.join(__dirname, "/backend/dist/server/server"));
 }
 
 // Endpoint to remotely shutdown the server
 function shutdown_server(){
-    fetch(`http://127.0.0.1:8080/shutdown-server`)
+    fetch(`http://127.0.0.1:8080/shutdown-server`);
 }
 
 // Recieve asynchronous request from renderer
@@ -67,9 +66,10 @@ ipcMain.handle('main', (event, arg) => {
 	break;
 	
     default:
-	console.log("command: "+command+" not found.");}
+	console.log("command: "+command+" not found.");
+    }
     
-})
+});
 
 function displayErrorMessage(error) {
     console.log(error);
@@ -84,10 +84,10 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
 	app.quit();
     }
-})
+});
 
 // When officially quitting the program, shut down
 // the server as well.
 app.on('before-quit', function () {
     shutdown_server();
-})
+});
